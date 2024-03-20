9e7e6b8472a0e93a887baa035d09e64fca3a4fb8
Faris <faris.gigic@stu.ibu.edu.ba>
2024-03-20 21:36:57 +0100

    Data Structures

--- src/main/java/Labs/Week4/LabTask/BinaryInsertionSort.java
+++ src/main/java/Labs/Week4/LabTask/BinaryInsertionSort.java
@@ -1,14 +1,33 @@
 package Labs.Week4.LabTask;
 
-import Labs.Week1.Task1.LinkedList;
+//import Labs.Week1.Task1.LinkedList;
+import Labs.Week4.LabTask.LinkedList;
 
 public class BinaryInsertionSort {
     public static <Data extends Comparable<Data>> void sort(LinkedList<Data> ll) {
-        // your code here
+        for(int i = 0; i < ll.count();i++){
+            Data key = ll.get(i);
+            int insertionPoint = findInsertionPoint(ll,i-1,key);
+            ll.add(insertionPoint,key);
+            }
+
     }
 
     public static <Data extends Comparable<Data>> int findInsertionPoint(LinkedList<Data> ll, int high, Data key) {
         // your code here (next line is a placeholder)
-        return 0;
+        int low = 0;
+        while(low <= high) {
+            int mid = low +( high - low ) / 2;
+            int cmp = key.compareTo(ll.get(mid));
+            if(cmp == 0){
+                return mid;
+            }else if(cmp < 0 ) {
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
+        }
+
+        return low;
     }
 }
