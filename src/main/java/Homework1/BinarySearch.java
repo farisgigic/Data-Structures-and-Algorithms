5502ae61b2818514e1ebcaebd7d82b61a639bc11
Faris <faris.gigic@stu.ibu.edu.ba>
2024-05-16 10:13:27 +0200

    Data Structures

---
+++ src/main/java/Homework1/BinarySearch.java
@@ -0,0 +1,9 @@
+package Homework1;
+
+import javax.swing.*;
+
+class BinarySearch {
+    public static int[] sort (Entry[] entries, String searchableName){
+        return null;
+    }
+}
---
+++ src/main/java/Homework1/Entry.java
@@ -0,0 +1,11 @@
+package Homework1;
+
+class Entry implements Comparable<Entry>{
+
+    //implement the relevant data attributes
+    @Override
+    public int compareTo(Entry other) {
+        // implement the actual compareTo logic (remove next line)
+        return 0;
+    }
+}
