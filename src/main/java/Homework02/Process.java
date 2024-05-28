af8e67ef81d0f9206c49e43181453f87575165f9
Faris <faris.gigic@stu.ibu.edu.ba>
2024-05-28 23:08:08 +0200

    Data Structures

--- src/main/java/Homework02/Process.java
+++ src/main/java/Homework02/Process.java
@@ -1,11 +1,58 @@
 package Homework02;
 
 public class Process implements Comparable<Process> {
-    // Implement the required attributes and constructor
+    private String processName;
+    private int priority;
+    private int burstTime;
+    private int arrivalTime;
 
-    @Override
+    public Process(String processName, int priority, int burstTime, int arrivalTime) {
+        this.processName = processName;
+        this.priority = priority;
+        this.burstTime = burstTime;
+        this.arrivalTime = arrivalTime;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public String getProcessName() {
+        return processName;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public int getArrivalTime() {
+        return this.arrivalTime;
+    }
+
+    public void setBurstTime(int burstTime) {
+        this.burstTime = burstTime;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public void setProcessName(String processName) {
+        this.processName = processName;
+    }
+
+    @Override // we are overriding this method from Comparable interface tj. implements Comparable<Process>
     public int compareTo(Process other) {
-        // implement the correct logic
-        return -1;
+
+        if (this.priority == other.priority) {
+            return Integer.compare(this.arrivalTime, other.arrivalTime);
+        }
+        return Integer.compare(this.priority, other.priority);
+
+        //Integer compare returns 3 results based on result : -1, 0 and 1
     }
 }
--- src/main/java/Homework02/ProcessQueue.java
+++ src/main/java/Homework02/ProcessQueue.java
@@ -1,25 +1,80 @@
 package Homework02;
+// we need to implement many methods to help us
 
 public class ProcessQueue {
     public Process[] pq = new Process[2];
     public int length = 0;
 
-    /* Add a new process into the priority queue */
     public void addProcess(Process process) {
-        // your code here
+
+        if (pq.length == length + 1) {
+            resize(2 * pq.length); // if array is full it doubles the size of the array by calling resize()
+        }
+        pq[++length] = process; // first we increment length and then process is inserted at the new process
+        swim(length);
     }
 
-    /* Return and remove the next Process that should be run */
     public Process runNextProcess() {
-        // your code here (next line is a placeholder)
-        return null;
+
+        Process min = pq[1]; // min process which is root of the heap at index 1
+        swap(1, length--); // process at root is swapped with the last process, and after it is swapped length is decremented (length--)
+        sink(1);  // this method moves the swapped process down the heap if necessary.
+        pq[length + 1] = null; // last element is set to null da se ne izgubi negdje
+        return min; // returns the process that was initially at the root
     }
 
-    /* Return the next Process that should be run (but do not delete it) */
     public Process peekNextProcess() {
-        // your code here (next line is a placeholder)
-        return null;
+
+        return pq[1]; // peeking first element
+    }
+    public boolean isEmpty() {
+        //    if(length == 0 ){
+        //      return true;
+        //    }
+        //    return false;
+        return (length == 0);
     }
 
-    /* Implement any other helper methods, if you need them. */
+    public void resize(int capacity) {
+        Process[] copy = new Process[capacity];
+        for (int i = 1; i <= length; i++) {
+            copy[i] = pq[i];
+        }
+        pq = copy;
+    }
+
+    private void swim(int k) { // code from lab with small modifications, here we need bigger element not smaller, we will use bigger() instead of less() method
+        while (k > 1 && bigger(k / 2, k)) {
+            swap(k, k / 2);
+            k = k / 2;
+        }
+    }
+
+    private void sink(int k) { // code from lab
+        while (2 * k <= length) {
+            int j = 2 * k;
+            if (j < length && bigger(j, j + 1)) {
+                j++;
+            }
+            if (!bigger(k, j)) {
+                break;
+            }
+            swap(k, j);
+            k = j;
+        }
+    }
+    private boolean less(int a, int b) { // code from lab
+
+        return (pq[a].compareTo(pq[b]) < 0);
+    }
+    private boolean bigger(int a, int b) { // code from lab; modified
+
+        return (pq[a].compareTo(pq[b]) > 0);
+    }
+
+    private void swap(int a, int b) { // method for swapping elements
+        Process newOne = pq[a];
+        pq[a] = pq[b];
+        pq[b] = newOne;
+    }
 }
